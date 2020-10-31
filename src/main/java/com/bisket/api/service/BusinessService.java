8f8689de362cf481afc3d4a218592bf1a19d1aa9
soohy <tngus90!>
2020-10-31 10:42:18 +0900

    chore: 로그, 주석 추가

--- src/main/java/com/bisket/api/service/BusinessService.java
+++ src/main/java/com/bisket/api/service/BusinessService.java
@@ -21,15 +21,19 @@ public class BusinessService {
         this.businessRepository = businessRepository;
     }
 
+    /**
+     * 검색조건을 만족하는 업체 목록을 페이지 단위로 조회한다
+     */
     public ResponsePageDto<List<BusinessDto.BusinessListPageGetResponseDto>> getBusinessListByPage(
             BusinessDto.BusinessListPageGetRequestDto searchConditions
     ) {
+        log.info("getBusinessListByPage() Call");
         ResponsePageDto<List<BusinessDto.BusinessListPageGetResponseDto>> response = new ResponsePageDto<List<BusinessDto.BusinessListPageGetResponseDto>>();
         Page<BusinessDto.BusinessListPageGetResponseDto> businessListPage = null;
         try {
             businessListPage = businessRepository.findBusinessListPageBySearchConditions(searchConditions);
         } catch (Exception e) {
-            log.error("getBusinessListByPage() Error!!\n{}", e);
+            log.error("getBusinessListByPage() Error\n{}", e);
             return response;
         }
 
