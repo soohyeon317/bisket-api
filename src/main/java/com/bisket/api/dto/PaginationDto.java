42a27e1cc06808a072d451cb7ba3272753f7c9b7
soohy <tngus90!>
2020-10-31 10:11:13 +0900

    chore: build warning 수정

--- src/main/java/com/bisket/api/dto/PaginationDto.java
+++ src/main/java/com/bisket/api/dto/PaginationDto.java
@@ -3,12 +3,14 @@ package com.bisket.api.dto;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 public class PaginationDto {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    @EqualsAndHashCode(callSuper = false)
     public static class PaginationRequestDto {
         private int currentPageNumber = 1;
         @ApiParam(hidden = true)
@@ -18,6 +20,7 @@ public class PaginationDto {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    @EqualsAndHashCode(callSuper = false)
     public static class PaginationResponseDto extends PaginationRequestDto {
         private int pageCount;
     }
--- src/main/java/com/bisket/api/dto/ResponsePageDto.java
+++ src/main/java/com/bisket/api/dto/ResponsePageDto.java
@@ -1,10 +1,12 @@
 package com.bisket.api.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class ResponsePageDto<T> extends ResponseDto<T> {
     private final PaginationDto.PaginationResponseDto pagination = new PaginationDto.PaginationResponseDto();
 }
