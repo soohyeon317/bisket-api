9edff093b523b75ce95b0b5baaabd455425789fb
soohy <tngus90!>
2020-11-01 23:56:00 +0900

    chore: Change entity name

--- src/main/java/com/bisket/api/repository/TourCruiserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bisket.api.repository;
-
-import com.bisket.api.entity.TourCruiser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TourCruiserRepository extends JpaRepository<TourCruiser, Long> {
-}
